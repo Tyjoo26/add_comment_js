2b204f149b0e179bb23cea4b31d4f09237874052
Tyjoo26 <Tyjoo21@gmail.com>
2018-03-12 08:10:59 -0600

    Cant figure this out

--- source/addComment.js
+++ source/addComment.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
   $('#new_comment_button').click(function(e){
     e.preventDefault();
-    
+    if($("#new_comment").is(":visible")){
+      $("#new_comment").hide();
+    } else {
+      $("#new_comment").show();
+    }
   })
 })
